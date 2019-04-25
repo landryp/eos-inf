abea38e4d9fa44a95343f32ec515bfecde1360a1
Phil <plandry@uoguelph.ca>
2019-04-25 16:51:05 -0500

    Debugging local

--- eosinf/drawmass.py
+++ eosinf/drawmass.py
@@ -9,7 +9,9 @@ def drawmass_uniform(mrange): #=[1.16,1.36,1.36,1.60]):
 	m1i, m1f, m2i, m2f = mrange # specify prior ranges
 	
 	m2 = (m2f-m2i)*np.random.random_sample()+m2i
-	m1i = max(m1i,m2) # enforce m2 < m1
 	m1 = (m1f-m1i)*np.random.random_sample()+m1i
+	
+	m1out = max(m1,m2)
+	m2out = min(m1,m2) # enforce m2 < m1
 
-	return m1, m2
+	return m1out, m2out
