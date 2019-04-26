60ca061e61a4007cf2ef6c546822ac5ecbbe961e
Phil <plandry@uoguelph.ca>
2019-04-25 23:11:31 -0500

    Adding crust marginalization, Mc windowing

--- eosinf/drawmass.py
+++ eosinf/drawmass.py
@@ -4,12 +4,17 @@ import numpy as np
 
 # Random draws from uniform prior in each component mass, with m2 < m1
 
-def drawmass_uniform(mrange): #=[1.16,1.36,1.36,1.60]):
+def drawmass(mrange,Mcut='False'):
 
-	m1i, m1f, m2i, m2f = mrange # specify prior ranges
+	while Mcut != True:	 
+
+		m1i, m1f, m2i, m2f = mrange # specify prior ranges, e.g. [1.16,1.36,1.36,1.60]
 	
-	m2 = (m2f-m2i)*np.random.random_sample()+m2i
-	m1 = (m1f-m1i)*np.random.random_sample()+m1i
+		m2 = (m2f-m2i)*np.random.random_sample()+m2i
+		m1 = (m1f-m1i)*np.random.random_sample()+m1i
+	
+		Mc = (m1*m2)**0.6/(m1+m2)**0.2
+		if Mc <= Mcut[1] and Mc >= Mcut[0]: Mcut = True
 	
 	m1out = max(m1,m2)
 	m2out = min(m1,m2) # enforce m2 < m1
