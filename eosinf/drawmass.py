217244b6921cdb9930921f41d209aa53464ac4a2
Phil <plandry@uoguelph.ca>
2019-06-11 14:23:20 -0500

    Fixing mass sampling to be truly uniform

--- eosinf/drawmass.py
+++ eosinf/drawmass.py
@@ -12,9 +12,11 @@ def drawmass(mrange,Mcut=[0.,1e10]): # random draws from uniform prior in each c
 		m2i, m2f, m1i, m1f = mrange # specify prior ranges, e.g. [1.16,1.36,1.36,1.60]
 	
 		m2 = (m2f-m2i)*np.random.random_sample()+m2i
-		m1 = (m1f-max(m1i,m2))*np.random.random_sample()+max(m1i,m2)
+		m1 = (m1f-m1i)*np.random.random_sample()+m1i
 	
-		Mc = (m1*m2)**0.6/(m1+m2)**0.2
-		if Mc <= Mcut[1] and Mc >= Mcut[0]: Mcok = True
+		if m1 >= m2:
 	
+			Mc = (m1*m2)**0.6/(m1+m2)**0.2
+			if Mc <= Mcut[1] and Mc >= Mcut[0]: Mcok = True
+
 	return m1, m2
