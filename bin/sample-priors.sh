5fe6b6d8c4628551b21406d416d88c4b6159446d
Phil <philippe.landry@ligo.org>
2020-05-12 14:19:57 -0400

    parallelize sample-priors

--- bin/sample-priors.sh
+++ bin/sample-priors.sh
@@ -6,5 +6,6 @@ obstype=$3
 numeos=$4
 nummass=$5
 mbounds=$6
+randseed=$7
 
-sample-priors $eosbank $priorpath -O $obstype -n $numeos -N $nummass -m $mbounds -v
+sample-priors $eosbank $priorpath -O $obstype -n $numeos -N $nummass -m $mbounds --randseed $randseed -v
