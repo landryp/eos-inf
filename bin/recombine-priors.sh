9935b6a297ca81ce57fb5fc6172a95b0bfc4474e
Philippe Landry <philippe.landry@ldas-pcdev6.ligo-wa.caltech.edu>
2020-05-12 21:10:06 -0700

    working remotely

--- bin/recombine-priors.sh
+++ bin/recombine-priors.sh
@@ -12,26 +12,13 @@ fi
 
 suffix=$(printf "%06d" 0)
 head -n 1 $priorcsvpath$suffix > $priorcsvpath
-touch $priorcsvpath.tmp
 
 for i in $(seq 0 $(($chunks-1)))
 do
 	suffix=$(printf "%06d" $i)
-	tail -n +2 $priorcsvpath$suffix >> $priorcsvpath.tmp
+	tail -n +2 $priorcsvpath$suffix >> $priorcsvpath
 	rm $priorcsvpath$suffix
 done
-
-eoslist=()
-while IFS=, read -r eos other
-do
-	counts=$(grep -o "$eos" <<< ${eoslist[*]} | wc -l)
-	if [ "$counts" -le "$nummass" ]; then
-		echo "$eos,$other" >> $priorcsvpath
-		eoslist+=("$eos")
-	fi
-done < $priorcsvpath.tmp
-
-rm $priorcsvpath.tmp
 
 chunksize=$((100*$nummass))
 numlines=$(wc -l < $priorcsvpath)
