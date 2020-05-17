c503e562faa0cfc2192ddafd0f832cdd788d88da
Phil <philippe.landry@ligo.org>
2020-05-17 14:10:16 -0400

    test for cbc

--- bin/weigh-samples.sh
+++ bin/weigh-samples.sh
@@ -10,7 +10,7 @@ if test -f "$priorcsvpath"; then
 	if [ "$props" == "m R" ]; then
 		weigh-samples $likepath $priorcsvpath $postpath $props -v --bandwidth=$bw --weight-column Prior --weight-column-is-log Prior --invert-weight-column Prior
 	elif [ "$props" == "m1 m2 Lambda1 Lambda2" ]; then
-		weigh-samples $likepath $priorcsvpath $postpath $props -v --bandwidth=$bw --weight-column Prior --weight-column-is-log Prior --invert-weight-column Prior --reflect --column-range lambda1 0 5000 --column-range lambda2 0 5000 --prune
+		weigh-samples $likepath $priorcsvpath $postpath $props -v --bandwidth=$bw --weight-column Prior --weight-column-is-log Prior --invert-weight-column Prior --reflect --column-range Lambda1 0 5000 --column-range Lambda2 0 5000 --prune
 	elif [ "$props" == "m" ]; then
 		cumweigh-samples $likepath $priorcsvpath $postpath "m" "Mmax" 0.1 3.5 -v
 	else
