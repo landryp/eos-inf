72203c844624205c05ff55f1732b5c9d3ca42d50
Phil <philippe.landry@ligo.org>
2020-05-20 09:39:13 -0400

    remove Mmax cut

--- bin/sample-priors.sh
+++ bin/sample-priors.sh
@@ -8,4 +8,4 @@ nummass=$5
 mbounds=$6
 chunkdat=$7
 
-sample-priors $eosbank $priorpath -O $obstype -n $numeos -N $nummass -m $mbounds --dag $chunkdat -v
+sample-priors $eosbank $priorpath -O $obstype -n $numeos -N $nummass -m $mbounds --dag $chunkdat -M 0.8,0.1 -v
